ae279d4db0dde0080723b71c01414528e00c9f21
rubuyathimona <103255225+rubuyathimona@users.noreply.github.com>
2022-04-14 14:56:00 +0300

    solution to the problem no:1035

---
+++ 1035.js
@@ -0,0 +1,11 @@
+let a = parseInt(line[0]);
+let b = parseInt(line[1]);
+let c = parseInt(line[2]);
+let d = parseInt(line[3]);
+
+if(b > c && d > a && c + d > a + b && c > 0 && d > 0 && a % 2 === 0){
+    console.log("Valores aceitos");
+}
+else {
+    console.log("Valores nao aceitos");
+}
