261456fb2095787973301b51932c7af9e0826594
rubuyathimona <103255225+rubuyathimona@users.noreply.github.com>
2022-04-21 16:24:54 +0300

    solution to the problem no:1050

---
+++ 1050.js
@@ -0,0 +1,31 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+let x = parseInt(lines[0]);
+if(x === 61){
+    console.log("Brasilia");
+}
+else if(x === 71){
+    console.log("Salvador");
+}
+else if(x === 11){
+    console.log("Sao Paulo");
+}
+else if(x === 21){
+    console.log("Rio de Janeiro");
+}
+else if(x === 32){
+    console.log("Juiz de Fora");
+}
+else if(x === 19){
+    console.log("Campinas");
+}
+else if(x === 27){
+    console.log("Vitoria");
+}
+else if(x === 31){
+    console.log("Belo Horizonte");
+}
+else{
+    console.log("DDD nao cadastrado");
+}
